bcc15cebc195a9998f92d892bac76779a5842c67
Pakizamutallim <127186291+Pakizamutallim@users.noreply.github.com>
2023-05-03 12:35:41 +0400

    Add files via upload

---
+++ qara div/assets/js/main.js
@@ -0,0 +1,9 @@
+// alert()
+
+const buton = document.getElementById("btn");
+
+function stretch() {
+    const bDiv = document.getElementById("blackDiv"); 
+    bDiv.style.width = "80%"; 
+    // bDiv.style.width = "20%"
+} 
