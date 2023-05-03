49deadf831b66cd239c8b619b8c15783b55a6a2b
Pakizamutallim <127186291+Pakizamutallim@users.noreply.github.com>
2023-05-04 01:40:17 +0400

    Add files via upload

---
+++ london tokyo/assets/js/main.js
@@ -0,0 +1,8 @@
+const butonlar = document.getElementById("btn"); 
+
+function openInfo() {
+    const london = document.getElementById("london"); 
+
+    const texts = document.getElementsByClassName("text"); 
+    texts.style.display = "block"; 
+}
